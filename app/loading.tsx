3f8d01b207a76c25b37c93914ab7d58562785203
https://github.com/Yashvir835/Prakartik-Ahaar <singhyashvir835@gmail.com>
2025-01-03 12:21:38 +0530

    added the new loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import classes from './loading.module.css'
+import { Loader } from 'lucide-react'
+
 function loading() {
   return (
-    <div className='text-4xl'>
-      <p className={classes.loading}>
-       Fetching data...
-</p>
+    <div className='flex flex-col justify-center items-center  mt-10'>
+      <div className='flex flex-row items-center '>
+        <Loader className="animate-spin h-20 w-20 text-gray-500" strokeWidth={1} viewBox="0 0 24 24" />
+        <span className="text-gray-500 ml-3">Loading...</span>
+      </div>
     </div>
   )
 }
