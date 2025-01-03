8b9ef62f304f93aab9cfbe103f3cdc7a063a1609
https://github.com/Yashvir835/Prakartik-Ahaar <singhyashvir835@gmail.com>
2025-01-03 21:01:13 +0530

    added eslint in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: { domains: ['api.slingacademy.com'], },
+  eslint: {
+      ignoreDuringBuilds: false,
+    },
 };
 
 export default nextConfig;
